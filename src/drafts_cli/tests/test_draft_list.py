9e1bc13d686ce714b603ee4fc0e985a9ed4b9af6
huynggg <huyanguyen3695@gmail.com>
2024-12-28 21:33:43 -0600

    added tests for draft lists and github workflows

---
+++ src/drafts_cli/tests/test_draft_list.py
@@ -0,0 +1,96 @@
+import os
+import sys
+# To resolve the ImportError issues by adding the project abs path to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+from app import DraftsApp
+from database import Draft, initialize_db
+
+
+@pytest.fixture
+def setup_test_db():
+    """Set up an in-memory test database."""
+    db = initialize_db('drafts_test.db')  # Configure `db` for testing
+    yield  # Test cases execute here
+    db.drop_tables([Draft])  # Clean up after tests
+    db.close()
+
+
+@pytest.mark.asyncio
+async def test_add_new_draft(setup_test_db):
+    app = DraftsApp()
+    async with app.run_test() as pilot:
+        draft_list = app.side_bar.draft_list
+        # Number of ListItem should also be 0
+        assert len(draft_list.children) == 0
+
+        # Now add a draft then count
+        await pilot.press("ctrl+n")
+        await pilot.press("t", "e", "s", "t")
+        await pilot.press("ctrl+s")
+        assert len(draft_list.children) == 1
+
+        # Add another one
+        await pilot.press("ctrl+n")
+        await pilot.press("a", "b", "c")
+        await pilot.press("ctrl+s")
+        assert len(draft_list.children) == 2
+
+
+@pytest.mark.asyncio
+async def test_delete_draft(setup_test_db):
+    app = DraftsApp()
+    async with app.run_test() as pilot:
+        draft_list = app.side_bar.draft_list
+        # Number of ListItem should also be 0
+        assert len(draft_list.children) == 0
+
+        # Now add a draft then count
+        await pilot.press("ctrl+n")
+        await pilot.press("t", "e", "s", "t")
+        await pilot.press("ctrl+s")
+        assert len(draft_list.children) == 1
+
+        # Now delete the item & count
+        await pilot.press("escape")
+        await pilot.press("down")  # select the first
+        await pilot.press("ctrl+d")
+        await pilot.press("n")
+        # Should still 1 since we chose No
+        assert len(draft_list.children) == 1
+        # Try again, chose Yes
+        await pilot.press("ctrl+d")
+        await pilot.press("y")
+        assert len(draft_list.children) == 0
+
+
+@pytest.mark.asyncio
+async def test_selecting_draft(setup_test_db):
+    app = DraftsApp()
+    async with app.run_test() as pilot:
+        draft_list = app.side_bar.draft_list
+        # Number of ListItem should also be 0
+        assert len(draft_list.children) == 0
+        # Now add a new draft
+        await pilot.press("ctrl+n")
+        await pilot.press("t", "e", "s", "t")
+        await pilot.press("ctrl+s")
+        # Add another one
+        await pilot.press("ctrl+n")
+        await pilot.press("a", "b", "c")
+        await pilot.press("ctrl+s")
+
+        # Focus the draft List then pick the second one
+        await pilot.press("escape")
+        await pilot.press("down", "down")  # select the 2nd
+        await pilot.press("enter")
+        # Should be the first one added
+        assert app.editor.text == "test"
+        # Now escape then go down 1 to pick the latest
+        await pilot.press("escape")
+        await pilot.press("n")  # To escape without saving the current draft
+        # Need to go up since the highlighted item is currently 2nd
+        await pilot.press("up")  # select the 1st
+        await pilot.press("enter")
+        assert app.editor.text == "abc"
